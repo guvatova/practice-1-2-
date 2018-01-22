8a4b57e817eb052920c1d54450a438a22dd53e1c
Firuza Guvatova <firuzka98@yandex.ru>
2018-01-22 11:03:29 +0500

    first commit

---
+++ git1/git1/git1.cpp
@@ -0,0 +1,54 @@
+// git1.cpp: –æ–ø—Ä–µ–¥–µ–ª—è–µ—Ç —Ç–æ—á–∫—É –≤—Ö–æ–¥–∞ –¥–ª—è –∫–æ–Ω—Å–æ–ª—å–Ω–æ–≥–æ –ø—Ä–∏–ª–æ–∂–µ–Ω–∏—è.
+//
+
+#include "stdafx.h"
+#include <iostream>
+#include <stdlib.h>
+
+using namespace std;
+
+void selection(int *ar, int size)
+{
+	int i, j, mx, nmx;
+	for (i = 0; i < size - 1; i++)
+	{
+		mx = ar[i];
+		nmx = i;
+		for (j = i + 1; j < size; j++)
+		{
+			if (ar[j]<mx)
+			{
+				mx = ar[j];
+				nmx = j;
+			}
+		}
+		ar[nmx] = ar[i];
+		ar[i] = mx;
+	
+	}
+}
+
+int main()
+{
+	setlocale(LC_ALL,"Rus");
+	int size;
+	cout << "¬‚Â‰ËÚÂ ‡ÁÏÂ Ï‡ÒÒË‚‡: ";
+	cin >> size;
+	int *ar = new int[size];
+	for (int i = 0; i < size; i++)
+	{
+		ar[i] = rand() % 60 + 30;
+		cout << ar[i] << "\t";
+	}
+	selection(ar, size);
+	cout << "\n\nŒÚÒÓÚËÓ‚‡ÌÌ˚È Ï‡ÒÒË‚:\n";
+	for (int i = 0; i < size; i++)
+	{
+		cout << ar[i] << "\t";
+	}
+	system("pause");
+	return 0;
+
+}
+
+
