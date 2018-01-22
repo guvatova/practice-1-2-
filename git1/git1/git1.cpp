42ab4a1d8038d3adfd41ca5ca7f8c964c7ab58a4
Firuza Guvatova <firuzka98@yandex.ru>
2018-01-22 12:45:50 +0500

    third commit

--- git1/git1/git1.cpp
+++ git1/git1/git1.cpp
@@ -30,27 +30,58 @@ void selection(int *ar, int size)
 	}
 }
 
+void radix(int *ar, int *br, int *cr, int sizeC, int sizeAB)
+{
+	int i, k, n;
+	k = sizeC;
+	n = sizeAB;
+	for (i = 0; i <= k; i++)
+		cr[i] = 0;
+	for (i = 0; i < n; i++)
+		cr[ar[i]] += 1;
+	for (i = 1; i < k + 1; i++)
+		cr[i] += cr[i - 1];
+	for (i = n - 1; i >= 0; i--)
+	{
+		br[cr[ar[i]] - 1] = ar[i];
+		cr[ar[i]] -= 1;
+	}
+}
+
+
 int main()
 {
 	setlocale(LC_ALL,"Rus");
 	int size;
 	cout << "Введите размер массива: ";
 	cin >> size;
-
+	int sizeC = 100;
 	clock_t start = clock();
 	getchar();
 	int *ar = new int[size];
 	for (int i = 0; i < size; i++)
 	{
-		ar[i] = rand() % 60 + 30;
+		ar[i] = rand() % sizeC;
 		cout << ar[i] << "\t";
 	}
+	int *br = new int[size];
+	int *cr = new int[sizeC];
+	int sizeAB = size;
+
 	selection(ar, size);
 	cout << "\n\nОтсортированный массив:\n";
 	for (int i = 0; i < size; i++)
 	{
 		cout << ar[i] << "\t";
 	}
+
+	radix(ar, br, cr, sizeC, sizeAB);
+	cout << "\n\nОтсортированный массив:\n";
+	for (int i = 0; i < size; i++)
+	{
+		cout << br[i] << "\t";
+	}
+
 	clock_t end = clock();
 	double seconds = (double)(end - start) / CLOCKS_PER_SEC;
 	printf("\nThe time: %f seconds\n", seconds);
